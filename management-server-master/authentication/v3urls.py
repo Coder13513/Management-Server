41530c27d700db8f90f1b328c5b402b116b843df
AmanpreetKaur <amanpreet.leanvia@gmail.com>
2020-09-14 16:38:01 +0530

    save

---
+++ management-server-master/authentication/v3urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from authentication.v3views import (
+    RegistrationAPIView, LoginAPIView, ProfileView, LogoutAPIView, ValidateOTPAPIView, VerifyAccountAPIView,
+)
+
+app_name = 'authenticationv3'
+
+urlpatterns = [
+    path("register/", RegistrationAPIView.as_view(), name="register"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+    path("login/otp/", ValidateOTPAPIView.as_view(), name="otp_validater"),
+    path("verify/", VerifyAccountAPIView.as_view(), name="verify_account")
+]
